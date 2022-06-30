c2f0524ff9ad667409046de186629f3c3223e7db
iTzBrassy <itzbrassy@gmail.com>
2022-06-30 10:16:59 -0700

    server addition

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Getting root");
+});
+
+app.get("/profile", (req, res) => {
+  res.send("getting profile");
+});
+app.post("/profile", (req, res) => {
+    console.log(req.body);
+  const user = {
+    name: "Sally",
+    hobby: "Running",
+  };
+  res.send(user);
+});
+
+app.listen(3000);
